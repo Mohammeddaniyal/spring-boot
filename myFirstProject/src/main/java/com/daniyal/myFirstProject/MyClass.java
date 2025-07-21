cba6c1b948b61945ca92b45906115cfd0eb5b84b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-21 22:16:33 +0530

    Created a custom class

---
+++ myFirstProject/src/main/java/com/daniyal/myFirstProject/MyClass.java
@@ -0,0 +1,13 @@
+package com.daniyal.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
+}
