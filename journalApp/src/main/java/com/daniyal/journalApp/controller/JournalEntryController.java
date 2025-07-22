353a2af8ad1d0bca25a1bb12002d3f317db888fc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-22 18:33:42 +0530

    Created JournalEntryController

---
+++ journalApp/src/main/java/com/daniyal/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,27 @@
+package com.daniyal.journalApp.controller;
+
+import com.daniyal.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries=new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll()
+    {
+        return new ArrayList(journalEntries.values());
+    }
+
+    @PostMapping
+    public void createEntry(@RequestParam JournalEntry entry)
+    {
+        journalEntries.put(entry.getId(),entry);
+    }
+}
