2804c149e822ca7fb45aa203701bdd5eae38e054
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-22 17:53:31 +0530

    Created a controller

--- journalApp/src/main/java/com/daniyal/journalApp/controller/HealthCheck.java
+++ journalApp/src/main/java/com/daniyal/journalApp/controller/HealthCheck.java
@@ -1,4 +1,14 @@
 package com.daniyal.journalApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthResult()
+    {
+        return "Ok";
+    }
 }
