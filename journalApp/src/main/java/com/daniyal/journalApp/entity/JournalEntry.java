43b6141417af5c357e472e59f3df9c128f219865
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-23 14:50:36 +0530

    Changed data type of id from long to String

--- journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
@@ -1,15 +1,15 @@
 package com.daniyal.journalApp.entity;
 
 public class JournalEntry {
-    private Long id;
+    private String id;
     private String title;
     private String content;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
