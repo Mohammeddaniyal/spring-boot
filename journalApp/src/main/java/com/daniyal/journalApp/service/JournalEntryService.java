d797fa21a47ca898d2faa5f400f075474d057e06
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-23 15:09:20 +0530

    Created service for save entry

--- journalApp/src/main/java/com/daniyal/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/daniyal/journalApp/service/JournalEntryService.java
@@ -1,5 +1,17 @@
 package com.daniyal.journalApp.service;
 
+import com.daniyal.journalApp.entity.JournalEntry;
+import com.daniyal.journalApp.repository.JournalEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JournalEntryService {
 
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+    public void saveEntry(JournalEntry journalEntry)
+    {
+        journalEntryRepository.save(journalEntry);
+    }
 }
