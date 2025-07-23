85465e3fc43342d408d1e9ba2597e427b1b2e3fe
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-23 15:54:15 +0530

    Increased new property for Date

--- journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
@@ -4,18 +4,33 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+// lec 13 9:56(for more detailed explanation)
+//@Document is like, it tells the mongodb that this is the class(JournalEntry)
+//this entity is a mapped entity with the mongodb collection
+
 @Document(collection = "journal_entries")
 public class JournalEntry {
-    @Id
+    @Id // used to map as primary key
     private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
 
-    public String getId() {
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
