a1fd78ce5964f77f9fe37332a9bd31de00784cb6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-24 15:58:43 +0530

    Added Transactional part rollback

--- journalApp/src/main/java/com/daniyal/journalApp/JournalAppApplication.java
+++ journalApp/src/main/java/com/daniyal/journalApp/JournalAppApplication.java
@@ -1,9 +1,15 @@
 package com.daniyal.journalApp;
 
+import com.mongodb.client.MongoDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@EnableTransactionManagement
 @SpringBootApplication
 public class JournalAppApplication {
 
@@ -11,4 +17,9 @@ public class JournalAppApplication {
 		SpringApplication.run(JournalAppApplication.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager iCanNameItWhatever(MongoDatabaseFactory dbFactory)
+	{
+		return new MongoTransactionManager(dbFactory);
+	}
 }
