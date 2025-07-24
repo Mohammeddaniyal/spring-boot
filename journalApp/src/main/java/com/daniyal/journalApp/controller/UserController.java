f0ef496ac3192e4e991c80813cbddbb6b71d8ee9
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-24 13:01:08 +0530

    Writting implemenation for UserController

--- journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.daniyal.journalApp.controller;
 
+import com.daniyal.journalApp.entity.User;
+import com.daniyal.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public void createUser(@RequestBody User user)
+    {
+        userService.saveEntry(user);
+    }
+    @GetMapping
+    public List<User> getAllUsers()
+    {
+        return userService.getAll();
+    }
 }
