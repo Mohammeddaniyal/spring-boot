b1f3cc0b8c9efa4a44a2ed255db3f1b131ec4b27
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-24 13:26:24 +0530

    Forgot to @PutMapping

--- journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
@@ -5,12 +5,12 @@ import com.daniyal.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -26,13 +26,13 @@ public class UserController {
     {
         return userService.getAll();
     }
-
+    @PutMapping 
     public ResponseEntity<?> updateUser(@RequestBody User user)
     {
         User userInDb=userService.findByUsername(user.getUsername());
         if(userInDb!=null) {
             userInDb.setUsername(user.getUsername());
-            userInDb.setPassword(user.getPassword(););
+            userInDb.setPassword(user.getPassword());
             userService.saveEntry(userInDb);
             return new ResponseEntity<>(HttpStatus.OK);
         }
