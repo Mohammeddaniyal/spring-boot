31d24f99149ebe57dbd2a6a9ad79466059c8aeb7
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-24 12:48:21 +0530

    Created filds and annotated it

--- journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/daniyal/journalApp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.daniyal.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,43 +13,11 @@ import java.time.LocalDateTime;
 //this entity is a mapped entity with the mongodb collection
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id // used to map as primary key
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
--- journalApp/src/main/java/com/daniyal/journalApp/entity/User.java
+++ journalApp/src/main/java/com/daniyal/journalApp/entity/User.java
@@ -1,4 +1,20 @@
 package com.daniyal.journalApp.entity;
 
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
 public class User {
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
 }
