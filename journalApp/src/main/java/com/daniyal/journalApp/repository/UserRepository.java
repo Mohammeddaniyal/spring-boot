56f164a267c428460356da12541d5e9acc4bffa9
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-24 12:51:52 +0530

    Added method and extended the MongoRepository

--- journalApp/src/main/java/com/daniyal/journalApp/repository/UserRepository.java
+++ journalApp/src/main/java/com/daniyal/journalApp/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.daniyal.journalApp.repository;
 
-public interface UserRepository {
+import com.daniyal.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    public User findByUsername(String username);
 }
