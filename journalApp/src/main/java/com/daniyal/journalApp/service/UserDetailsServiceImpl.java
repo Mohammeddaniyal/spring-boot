14fe6e6e7ecedb5b18fed8e6390a73c872caa3fa
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-25 18:17:42 +0530

    In UserDetailsServiceImpl forgot to annotate it with @Component

--- journalApp/src/main/java/com/daniyal/journalApp/service/UserDetailsServiceImpl.java
+++ journalApp/src/main/java/com/daniyal/journalApp/service/UserDetailsServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-public class CustomUserDetailsServiceImpl implements UserDetailsService {
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
