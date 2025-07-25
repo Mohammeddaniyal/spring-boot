e82c9e1775ad0afd84061fd18974ce58d4ef03ab
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-25 19:01:37 +0530

    Added part to get upcoming username

--- journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/daniyal/journalApp/controller/UserController.java
@@ -5,6 +5,8 @@ import com.daniyal.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +23,8 @@ public class UserController {
     @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody User user)
     {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
         User userInDb=userService.findByUsername(username);
         if(userInDb!=null) {
             userInDb.setUsername(user.getUsername());
