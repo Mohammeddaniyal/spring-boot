798839283af8e34065e98607a22ca886540be3f1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-25 18:45:03 +0530

    Added roles in createUser

--- journalApp/src/main/java/com/daniyal/journalApp/service/UserService.java
+++ journalApp/src/main/java/com/daniyal/journalApp/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +23,7 @@ public class UserService {
     public void saveEntry(User user)
     {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("User"));
         userRepository.save(user);
     }
     public List<User> getAll()
