b655620d085a8043211cc0ec05d1d9edca0ed024
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-25 18:41:17 +0530

    Due to new version the old way to configure wasn't working,i learnt how to do it new way, the error i was getting about Cicular dependency, its like SecurityClass needs that dependecy and that dependency needs SecurityClass lol silly things

--- journalApp/src/main/java/com/daniyal/journalApp/config/SpringSecurity.java
+++ journalApp/src/main/java/com/daniyal/journalApp/config/SpringSecurity.java
@@ -4,11 +4,14 @@ import com.daniyal.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,10 +34,22 @@ public class SpringSecurity {
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception    {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        return userDetailsService;
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
